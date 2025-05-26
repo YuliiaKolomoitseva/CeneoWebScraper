ac1bcfefdf94f3492808afef27b49c8c94ca0d8b
Yuliia <juliakolomoitsevawork@gmail.com>
2025-05-26 15:35:18 +0200

    new 26.05

--- app/views.py
+++ app/views.py
@@ -1,18 +1,19 @@
 from app import app
-
-from flask import render_template
+from flask import render_template, redirect, url_for
+from app.forms import ExtractForm
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/extract")
+@app.route("/extract", methods=['POST'])
 def extract():
-    return render_template("extract.html")
+    return render_template("extract.html", form=form)
 
 @app.route("/products")
 def products():
-    return render_template("products.html")
+    return redirect(url_for("product", product_id=product_id))
+    return redirect(url_for())
 
 @app.route("/product/<product_id>")
 def product(product_id):

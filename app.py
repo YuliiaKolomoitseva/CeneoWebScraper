1027d22caa67b486312fc914806a2f79dfd5fa07
Yuliia <Yuliia>
2025-06-09 15:16:56 +0200

    new changes

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, send_file, abort
-import pandas as pd
 import os
-
-from utils import get_product_summaries  
+import tempfile
+import pandas as pd
+from flask import (
+    Flask, render_template, request, redirect, url_for,
+    flash, send_file, abort
+)
+from utils import get_product_summaries
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here' 
+app.secret_key = os.environ.get('SECRET_KEY', 'dev_secret_key')
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -17,7 +21,6 @@ def extract():
         if not product_id:
             flash('Please enter a product ID', 'error')
             return redirect(url_for('extract'))
-        
         
         opinions_path = f'app/static/opinions/{product_id}.json'
         if not os.path.exists(opinions_path):
@@ -50,18 +53,26 @@ def download_opinions(product_id, filetype):
         abort(404, description="File not found")
 
     df = pd.read_json(opinions_path)
-    filename = f'{product_id}.{filetype}'
 
-    if filetype == 'csv':
-        df.to_csv(filename, index=False)
-    elif filetype == 'xlsx':
-        df.to_excel(filename, index=False)
-    elif filetype == 'json':
-        df.to_json(filename, orient='records', indent=2)
-    else:
-        return "Unsupported file type", 400
+    with tempfile.NamedTemporaryFile(delete=False, suffix=f'.{filetype}') as tmp:
+        filename = tmp.name
+        if filetype == 'csv':
+            df.to_csv(filename, index=False)
+        elif filetype == 'xlsx':
+            df.to_excel(filename, index=False)
+        elif filetype == 'json':
+            df.to_json(filename, orient='records', indent=2)
+        else:
+            return "Unsupported file type", 400
 
-    return send_file(filename, as_attachment=True)
+    response = send_file(filename, as_attachment=True)
+    os.remove(filename)
+    return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', error=e), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
+
